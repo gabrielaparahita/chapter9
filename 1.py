c75b1bce70ff352ac23b0df89c9b030f926b7564
gabrielaparahita <gabrielaparahita@gmail.com>
2020-12-22 08:37:33 +0700

    chapter9
    9

---
+++ 1.py
@@ -0,0 +1,9 @@
+mate= ['M', 'A', 'T', 'E', 'M', 'A', 'T', 'I', 'K', 'A']
+print(mate)
+print('lalu diubah')
+
+mate[2]= 'S'
+mate[6]= 'S'
+print(mate)
+
+
---
+++ 2+3.py
@@ -0,0 +1,4 @@
+for k in range(4):
+    print(('*'*(1+2*k)).center(1+2*10))
+for k in reversed (range(4)):
+    print(('*'*(1+2*k)).center(1+2*10))
